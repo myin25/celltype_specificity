418ab4ba425283f060c60ce9b503da1c4a52a69d
myin25 <yinmelody8@gmail.com>
2023-06-27 21:27:44 +0000

    first commit

---
+++ download-data.sh
@@ -0,0 +1,229 @@
+# K562 H3K9me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF559MMQ/@@download/ENCFF559MMQ.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K9me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF812HRW/@@download/ENCFF812HRW.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K9me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF371GMJ/@@download/ENCFF371GMJ.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K9me3/peaks.bed
+
+# K562 H3K4me1
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF783QIW/@@download/ENCFF783QIW.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me1/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF761XBZ/@@download/ENCFF761XBZ.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me1/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF159VKJ/@@download/ENCFF159VKJ.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me1/peaks.bed
+
+# K562 H3K27me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF470HOG/@@download/ENCFF470HOG.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K27me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF928NWQ/@@download/ENCFF928NWQ.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K27me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF031FSF/@@download/ENCFF031FSF.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K27me3/peaks.bed
+
+# K562 H3K27ac
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF465GBD/@@download/ENCFF465GBD.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K27ac/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF381NDD/@@download/ENCFF381NDD.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K27ac/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF544LXB/@@download/ENCFF544LXB.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K27ac/peaks.bed
+
+# K562 H3K36me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF215NKF/@@download/ENCFF215NKF.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K36me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF440XMD/@@download/ENCFF440XMD.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K36me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF631VWP/@@download/ENCFF631VWP.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K36me3/peaks.bed
+
+# K562 H3K4me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF036HPL/@@download/ENCFF036HPL.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF525ZRM/@@download/ENCFF525ZRM.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF616DLO/@@download/ENCFF616DLO.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K4me3/peaks.bed
+
+# K562 K9ac
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF521KCV/@@download/ENCFF521KCV.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K9ac/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF937QUK/@@download/ENCFF937QUK.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K9ac/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF558JOB/@@download/ENCFF558JOB.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K9ac/peaks.bed
+
+# K562 K79me2
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF334HSS/@@download/ENCFF334HSS.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K79me2/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF544AVW/@@download/ENCFF544AVW.bigWig -O /users/myin25/projects/celltype_specificity/data/K562/H3K79me2/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF209OQD/@@download/ENCFF209OQD.bed.gz -O /users/myin25/projects/celltype_specificity/data/K562/H3K79me2/peaks.bed
+
+
+
+# Caco-2 H3K9me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF206MOP/@@download/ENCFF206MOP.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K9me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF904AUC/@@download/ENCFF904AUC.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K9me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF945SLD/@@download/ENCFF945SLD.bed.gz -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K9me3/peaks.bed
+
+# Caco-2 H3K4me1
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF111ZPB/@@download/ENCFF111ZPB.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me1/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF135XXE/@@download/ENCFF135XXE.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me1/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF095KMQ/@@download/ENCFF095KMQ.bed.gz -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me1/peaks.bed
+
+# Caco-2 K27me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF490XWX/@@download/ENCFF490XWX.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K27me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF468RGR/@@download/ENCFF468RGR.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K27me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF493YLO/@@download/ENCFF493YLO.bed.gz -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K27me3/peaks.bed
+
+# Caco-2 K27ac doesn't exist
+
+# Caco-2 H3K36me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF268DMD/@@download/ENCFF268DMD.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K36me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF141QAK/@@download/ENCFF141QAK.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K36me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF259LYY/@@download/ENCFF259LYY.bed.gz -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K36me3/peaks.bed
+
+# Caco-2 H3K4me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF732EDS/@@download/ENCFF732EDS.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF109LUV/@@download/ENCFF109LUV.bigWig -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF434CLI/@@download/ENCFF434CLI.bed.gz -O /users/myin25/projects/celltype_specificity/data/Caco-2/H3K4me3/peaks.bed
+
+# Caco-2 K9ac doesn't exist
+
+# Caco-2 K79me2 doesn't exist
+
+
+# A673 K9me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF079MGM/@@download/ENCFF079MGM.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K9me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF163YOI/@@download/ENCFF163YOI.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K9me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF735KYF/@@download/ENCFF735KYF.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K9me3/peaks.bed
+
+# A673 H3K4me1
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF493ATA/@@download/ENCFF493ATA.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me1/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF820QRK/@@download/ENCFF820QRK.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me1/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF738YYU/@@download/ENCFF738YYU.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me1/peaks.bed
+
+# A673 K27me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF306ADL/@@download/ENCFF306ADL.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K27me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF061CVO/@@download/ENCFF061CVO.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K27me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF455GYL/@@download/ENCFF455GYL.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K27me3/peaks.bed
+
+# A673 K27ac
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF290SUR/@@download/ENCFF290SUR.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K27ac/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF261CVS/@@download/ENCFF261CVS.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K27ac/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF979ACW/@@download/ENCFF979ACW.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K27ac/peaks.bed
+
+# A673 H3K36me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF003JSV/@@download/ENCFF003JSV.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K36me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF014WRA/@@download/ENCFF014WRA.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K36me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF723NVE/@@download/ENCFF723NVE.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K36me3/peaks.bed
+
+# A673 H3K4me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF775REM/@@download/ENCFF775REM.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF938RYF/@@download/ENCFF938RYF.bigWig -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF404PIP/@@download/ENCFF404PIP.bed.gz -O /users/myin25/projects/celltype_specificity/data/A673/H3K4me3/peaks.bed
+
+# A673 K9ac doesn't exist
+
+# A673 K79me2 doesn't exist
+
+
+# HUVEC- K9me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF952PCS/@@download/ENCFF952PCS.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF923UUB/@@download/ENCFF923UUB.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF971SSW/@@download/ENCFF971SSW.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9me3/peaks.bed
+
+# HUVEC- H3K4me1
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF604MQL/@@download/ENCFF604MQL.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me1/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF717SSQ/@@download/ENCFF717SSQ.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me1/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF148APW/@@download/ENCFF148APW.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me1/peaks.bed
+
+# HUVEC- K27me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF494NSX/@@download/ENCFF494NSX.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF993XMQ/@@download/ENCFF993XMQ.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF938HVH/@@download/ENCFF938HVH.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27me3/peaks.bed
+
+# HUVEC- K27ac
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF091FMS/@@download/ENCFF091FMS.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27ac/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF656TFQ/@@download/ENCFF656TFQ.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27ac/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF426QFH/@@download/ENCFF426QFH.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K27ac/peaks.bed
+
+# HUVEC- H3K36me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF375ESV/@@download/ENCFF375ESV.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K36me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF661CPQ/@@download/ENCFF661CPQ.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K36me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF159IYQ/@@download/ENCFF159IYQ.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K36me3/peaks.bed
+
+# HUVEC- H3K4me3
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF022OFM/@@download/ENCFF022OFM.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me3/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF669KWL/@@download/ENCFF669KWL.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me3/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF657MIQ/@@download/ENCFF657MIQ.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K4me3/peaks.bed
+
+# HUVEC- K9ac
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF810XOL/@@download/ENCFF810XOL.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9ac/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF929FMX/@@download/ENCFF929FMX.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9ac/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF392DCV/@@download/ENCFF392DCV.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K9ac/peaks.bed
+
+# HUVEC- K79me2
+# bigwig p-value
+wget https://www.encodeproject.org/files/ENCFF574IEQ/@@download/ENCFF574IEQ.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K79me2/pvalue.bigWig
+# bigwig fold-change
+wget https://www.encodeproject.org/files/ENCFF492JYB/@@download/ENCFF492JYB.bigWig -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K79me2/foldchange.bigWig
+# bed
+wget https://www.encodeproject.org/files/ENCFF351SWW/@@download/ENCFF351SWW.bed.gz -O /users/myin25/projects/celltype_specificity/data/HUVEC/H3K79me2/peaks.bed
